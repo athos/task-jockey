4f3487527790479c5731f9acf11964b791aebbd1
Shogo Ohta <athos0220@gmail.com>
2022-08-19 01:18:01 +0900

    Implement simple process spawning

--- src/task_jockey/task_jockey.clj
+++ src/task_jockey/task_jockey.clj
@@ -1,6 +1,28 @@
 (ns task-jockey.task-jockey)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(def state (atom {:tasks (sorted-map)}))
+(def children (atom (sorted-map)))
+
+(defn add-task [state task]
+  (let [next-id (or (some-> (rseq (:tasks state)) ffirst inc) 0)]
+    (assoc-in state [:tasks next-id] (assoc task :id next-id))))
+
+(defn next-task-id [state]
+  (->> (:tasks state)
+       (filter #(= (:status (val %)) :queued))
+       ffirst))
+
+(defn now [] (java.util.Date.))
+
+(defn start-process [state id]
+  (let [task (get-in state [:tasks id])
+        pb (ProcessBuilder. (:command task))
+        child (.start pb)]
+    (swap! children assoc id child)
+    (update-in state [:tasks id]
+               assoc :status :running :start (now))))
+
+(defn spawn-new [state]
+  (if-let [id (next-task-id state)]
+    (start-process state id)
+    state))
