b22e3dcd710991ff162b43809c1a6cf91fdb3840
Shogo Ohta <athos0220@gmail.com>
2022-08-20 00:09:30 +0900

    Make task handler hold state

--- src/task_jockey/task_jockey.clj
+++ src/task_jockey/task_jockey.clj
@@ -6,37 +6,37 @@
 
 (def state (volatile! (make-state)))
 
-(defn make-task-handler []
-  {:children {"default" (sorted-map)}})
+(defn make-task-handler [state]
+  {:state state
+   :children {"default" (sorted-map)}})
 
-(def task-handler (volatile! (make-task-handler)))
+(def task-handler (volatile! (make-task-handler state)))
 
 (defn add-task [state task]
   (let [next-id (or (some-> (rseq (:tasks state)) ffirst inc) 0)]
     (assoc-in state [:tasks next-id] (assoc task :id next-id))))
 
-(defn next-task-id [{:keys [children]} locked-state]
-  (->> (:tasks @locked-state)
+(defn next-task-id [{:keys [children state]}]
+  (->> (:tasks @state)
        (filter (fn [[_ {:keys [group] :as task}]]
                  (and (= (:status task) :queued)
                       (let [running-tasks (get children group)]
                         (< (count running-tasks)
-                           (get-in @locked-state
-                                   [:groups group :parallel-tasks]))))))
+                           (get-in @state [:groups group :parallel-tasks]))))))
        ffirst))
 
 (defn now [] (java.util.Date.))
 
-(defn start-process [task-handler locked-state id]
-  (let [task (get-in @locked-state [:tasks id])
+(defn start-process [task-handler id]
+  (let [task (get-in @(:state task-handler) [:tasks id])
         pb (ProcessBuilder. (:command task))
         child (.start pb)]
-    (vswap! locked-state update-in [:tasks id]
+    (vswap! (:state task-handler) update-in [:tasks id]
             assoc :status :running :start (now))
     (assoc-in task-handler [:children (:group task) id] child)))
 
-(defn spawn-new [task-handler state]
-  (locking state
-    (if-let [id (next-task-id task-handler state)]
-      (start-process task-handler state id)
+(defn spawn-new [task-handler]
+  (locking (:state task-handler)
+    (if-let [id (next-task-id task-handler)]
+      (start-process task-handler id)
       task-handler)))
