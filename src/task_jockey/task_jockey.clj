d60f4bcd0e2a28c9cb26ff40721871f68067fcd1
Shogo Ohta <athos0220@gmail.com>
2022-08-20 00:09:30 +0900

    Use locking for mutual exclusion

--- src/task_jockey/task_jockey.clj
+++ src/task_jockey/task_jockey.clj
@@ -1,28 +1,31 @@
 (ns task-jockey.task-jockey)
 
-(def state (atom {:tasks (sorted-map)}))
-(def children (atom (sorted-map)))
+(def state (volatile! {:tasks (sorted-map)}))
+(def task-handler
+  (volatile! {:children (sorted-map)}))
 
 (defn add-task [state task]
   (let [next-id (or (some-> (rseq (:tasks state)) ffirst inc) 0)]
     (assoc-in state [:tasks next-id] (assoc task :id next-id))))
 
-(defn next-task-id [state]
-  (->> (:tasks state)
-       (filter #(= (:status (val %)) :queued))
-       ffirst))
+(defn next-task-id [{:keys [children]} locked-state]
+  (when-not (seq children)
+    (->> (:tasks @locked-state)
+         (filter #(= (:status (val %)) :queued))
+         ffirst)))
 
 (defn now [] (java.util.Date.))
 
-(defn start-process [state id]
-  (let [task (get-in state [:tasks id])
+(defn start-process [task-handler locked-state id]
+  (let [task (get-in @locked-state [:tasks id])
         pb (ProcessBuilder. (:command task))
         child (.start pb)]
-    (swap! children assoc id child)
-    (update-in state [:tasks id]
-               assoc :status :running :start (now))))
+    (vswap! locked-state update-in [:tasks id]
+            assoc :status :running :start (now))
+    (assoc-in task-handler [:children id] child)))
 
-(defn spawn-new [state]
-  (if-let [id (next-task-id state)]
-    (start-process state id)
-    state))
+(defn spawn-new [task-handler state]
+  (locking state
+    (if-let [id (next-task-id task-handler state)]
+      (start-process task-handler state id)
+      task-handler)))
