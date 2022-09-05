0bad55f6d2012ddfd57fab1eeee475383a7d90d0
Shogo Ohta <athos0220@gmail.com>
2022-09-05 19:57:01 +0900

    Require explicit system start

--- src/task_jockey/repl.clj
+++ src/task_jockey/repl.clj
@@ -10,106 +10,113 @@
             [task-jockey.task-handler :as handler]
             [task-jockey.utils :as utils]))
 
-(defn- make-default-transport []
-  (transport/make-fn-transport message/handle-message))
+(def ^:private system nil)
 
-(def system
-  {:client (make-default-transport)
-   :loop (handler/start-loop system/state system/message-queue)})
+(defn- current-client []
+  (or (:client system)
+      (throw (ex-info "Call task-jockey.repl/start-system! first" {}))))
 
 (defn add [command & {:keys [work-dir after]}]
-  (let [res (client/add (:client system) command work-dir after)]
+  (let [res (client/add (current-client) command work-dir after)]
     (select-keys res [:task-id])))
 
 (defn status [& {:keys [group]}]
-  (let [res (client/status (:client system))]
+  (let [res (client/status (current-client))]
     (when (= (:type res) :status-response)
       (if group
         (state/print-single-group (:status res) group)
         (state/print-all-groups (:status res))))))
 
 (defn clean []
-  (client/clean (:client system))
+  (client/clean (current-client))
   nil)
 
 (defn stash [id-or-ids]
   (let [task-ids (utils/->coll id-or-ids)
-        res (client/stash (:client system) task-ids)]
+        res (client/stash (current-client) task-ids)]
     (println (:message res))))
 
 (defn enqueue [id-or-ids]
   (let [task-ids (utils/->coll id-or-ids)
-        res (client/enqueue (:client system) task-ids)]
+        res (client/enqueue (current-client) task-ids)]
     (println (:message res))))
 
 (defn switch [task-id1 task-id2]
-  (let [res (client/switch (:client system) task-id1 task-id2)]
+  (let [res (client/switch (current-client) task-id1 task-id2)]
     (println (:message res))))
 
 (defn restart [id-or-ids]
   (let [task-ids (utils/->coll id-or-ids)
-        res (client/restart (:client system) task-ids)]
+        res (client/restart (current-client) task-ids)]
     (println (:message res))))
 
 (defn edit [task-id command]
-  (client/edit (:client system) task-id command)
+  (client/edit (current-client) task-id command)
   nil)
 
 (defn log
   ([] (log []))
   ([id-or-ids]
    (let [task-ids (utils/->coll id-or-ids)
-         res (client/log (:client system) task-ids)]
+         res (client/log (current-client) task-ids)]
      (log/print-logs (:tasks res) task-ids))))
 
 (defn follow [id]
   (log/follow-logs system/state id))
 
 (defn send [task-id input]
-  (client/send (:client system) task-id input)
+  (client/send (current-client) task-id input)
   nil)
 
 (defn kill [id-or-ids]
   (let [task-ids (utils/->coll id-or-ids)
-        res (client/kill (:client system) task-ids)]
+        res (client/kill (current-client) task-ids)]
     (println (:message res))))
 
 (defn parallel [n & {:keys [group]}]
-  (let [res (client/parallel (:client system) group n)]
+  (let [res (client/parallel (current-client) group n)]
     (println (:message res))))
 
 (defn groups []
-  (let [res (client/groups (:client system))]
+  (let [res (client/groups (current-client))]
     (doseq [[name group] (:groups res)]
       (state/print-group-summary name group)
       (newline))))
 
 (defn group-add [name & {:keys [parallel]}]
-  (let [res (client/group-add (:client system) name parallel)]
+  (let [res (client/group-add (current-client) name parallel)]
     (println (:message res))))
 
 (defn group-rm [name]
-  (let [res (client/group-rm (:client system) name)]
+  (let [res (client/group-rm (current-client) name)]
     (println (:message res))))
 
-(defn start-server! [& {:keys [host port]
-                       :or {host "localhost" port 5555}
-                       :as opts}]
-  (let [opts' (assoc opts :host host :port port)
-        fut (future (handler/start-loop system/state
-                                        system/message-queue))
-        server (server/start-server opts')
-        client (transport/make-socket-transport opts')]
-    (alter-var-root #'system
-                    (constantly {:loop fut :server server :client client}))
-    nil))
+(defn- stop-system* [system]
+  (.close (current-client))
+  (when-let [server (:server system)]
+    (server/stop-server server))
+  (when-let [fut (:loop system)]
+    (future-cancel fut))
+  nil)
 
-(defn stop-server! []
-  (alter-var-root #'system
-                  (fn [system]
-                    (.close (:client system))
-                    (when-let [server (:server system)]
-                      (server/stop-server server))
-                    (when-let [fut (:loop system)]
-                      (future-cancel fut))
-                    {:client (make-default-transport)})))
+(defn stop-system! []
+  (alter-var-root #'system stop-system*)
+  :stopped)
+
+(defn- ensure-stopped [system]
+  (when system
+    (stop-system* system)))
+
+(defn start-system! [& {:keys [port] :as opts}]
+  (letfn [(start! [system]
+            (ensure-stopped system)
+            (let [fut (future
+                        (handler/start-loop system/state system/message-queue))
+                  [server client]
+                  (if port
+                    [(server/start-server opts)
+                     (transport/make-socket-transport opts)]
+                    [nil (transport/make-fn-transport message/handle-message)])]
+              {:loop fut :server server :client client}))]
+    (alter-var-root #'system start!)
+    :started))
