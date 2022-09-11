81b2faff2ce6388f63b1d230dd459eb5007acad5
Shogo Ohta <athos0220@gmail.com>
2022-09-11 19:41:49 +0900

    Refactor task handler into imperative impl

--- src/task_jockey/task_handler.clj
+++ src/task_jockey/task_handler.clj
@@ -8,13 +8,13 @@
 (defn make-task-handler [state queue]
   {:state state
    :queue queue
-   :children {"default" (sorted-map)}})
+   :children (volatile! {"default" (sorted-map)})})
 
 (defn- next-task-id [{:keys [children state]}]
   (->> (:tasks @state)
        (filter (fn [[_ {:keys [group] :as task}]]
                  (and (= (:status task) :queued)
-                      (let [running-tasks (get children group)]
+                      (let [running-tasks (get @children group)]
                         (< (count running-tasks)
                            (get-in @state [:groups group :parallel-tasks])))
                       (let [deps (:dependencies task)]
@@ -23,7 +23,7 @@
        ffirst))
 
 (defn- next-group-worker [{:keys [children]} group]
-  (let [pool (get children group)]
+  (let [pool (get @children group)]
     (or (->> pool
              (keep-indexed (fn [i [worker-id _]]
                              (when (not= i worker-id)
@@ -51,95 +51,91 @@
                   (.start))]
     (vswap! (:state task-handler) update-in [:tasks id]
             assoc :status :running :start (now))
-    (assoc-in task-handler [:children (:group task) worker-id]
-              {:task id :child child})))
+    (vswap! (:children task-handler) assoc-in
+            [(:group task) worker-id]
+            {:task id :child child})))
 
 (defn- spawn-new [task-handler]
   (locking (:state task-handler)
-    (loop [handler task-handler]
-      (if-let [id (next-task-id handler)]
-        (recur (start-process handler id))
-        handler))))
+    (loop []
+      (when-let [id (next-task-id task-handler)]
+        (start-process task-handler id)
+        (recur)))))
 
-(defn- handle-messages [{:keys [state] :as task-handler}]
-  (if-let [msg (queue/pop-message! (:queue task-handler))]
+(defn- handle-messages [{:keys [state queue children]}]
+  (when-let [msg (queue/pop-message! queue)]
     (case (:type msg)
       :group-add
       (do (locking state
             (vswap! state assoc-in [:groups (:name msg)]
                     {:parallel-tasks (:parallel-tasks msg)
                      :status :running}))
-          (assoc-in task-handler [:children (:name msg)] (sorted-map)))
+          (vswap! children assoc (:name msg) (sorted-map)))
       :group-remove
       (do (locking state
             (vswap! state update :groups dissoc (:name msg)))
-          (update task-handler :children dissoc (:name msg)))
+          (vswap! children dissoc (:name msg)))
       :send
       (let [{:keys [task-id input]} msg
-            child (->> (for [[_ pool] (:children task-handler)
+            child (->> (for [[_ pool] @children
                              [_ {:keys [task child]}] pool
                              :when (= task task-id)]
                          child)
                        first)]
         (doto (.getOutputStream ^Process child)
           (.write (.getBytes ^String input))
-          (.flush))
-        task-handler)
+          (.flush)))
       :kill
       (locking state
         (doseq [task-id (:task-ids msg)
-                :let [child (->> (for [[_ pool] (:children task-handler)
+                :let [child (->> (for [[_ pool] @children
                                        [_ {:keys [task child]}] pool
                                        :when (= task-id task)]
                                    child)
                                  first)]]
           (vswap! state update-in [:tasks task-id] assoc
                   :status :killed :end (now))
-          (.destroy ^Process child))
-        task-handler))
-    task-handler))
+          (.destroy ^Process child))))))
 
-(defn- handle-finished-tasks [{:keys [state] :as task-handler}]
-  (let [finished (for [[group pool] (:children task-handler)
+(defn- handle-finished-tasks [{:keys [state children]}]
+  (let [finished (for [[group pool] @children
                        [worker {:keys [task ^Process child]}] pool
                        :when (not (.isAlive child))]
                    [group worker task (.exitValue child)])]
-    (if (seq finished)
-      (do (locking state
-            (->> finished
-                 (reduce (fn [state [_ _ task code]]
-                           (cond-> state
-                             (not= (:status (get-in state [:tasks task]))
-                                   :killed)
-                             (update-in [:tasks task] assoc
-                                        :status (if (= code 0) :success :failed)
-                                        :code code
-                                        :end (now))))
-                         @state)
-                 (vreset! state)))
-          (reduce (fn [handler [group worker _ _]]
-                    (update-in handler [:children group] dissoc worker))
-                  task-handler
-                  finished))
-      task-handler)))
+    (when (seq finished)
+      (locking state
+        (->> finished
+             (reduce (fn [state [_ _ task code]]
+                       (cond-> state
+                         (not= (:status (get-in state [:tasks task]))
+                               :killed)
+                         (update-in [:tasks task] assoc
+                                    :status (if (= code 0) :success :failed)
+                                    :code code
+                                    :end (now))))
+                     @state)
+             (vreset! state)))
+      (doseq [[group worker _ _] finished]
+        (vswap! children update group dissoc worker)))))
 
-(defn- enqueue-delayed-tasks [{:keys [state] :as task-handler}]
+(defn- enqueue-delayed-tasks [{:keys [state]}]
   (locking state
     (doseq [[id task] (:tasks @state)
             :when (and (= (:status task) :stashed)
                        (when-let [^Date t (:enqueue-at task)]
                          (.before t (now))))]
-      (vswap! state assoc-in [:tasks id :status] :queued)))
-  task-handler)
+      (vswap! state assoc-in [:tasks id :status] :queued))))
 
 (defn- step [handler]
-  (-> handler
-      (handle-messages)
-      (handle-finished-tasks)
-      (enqueue-delayed-tasks)
-      (spawn-new)))
+  (doto handler
+    (handle-messages)
+    (handle-finished-tasks)
+    (enqueue-delayed-tasks)
+    (spawn-new)))
 
 (defn start-loop [state queue]
-  (loop [handler (make-task-handler state queue)]
-    (Thread/sleep 200)
-    (recur (step handler))))
+  (let [handler (make-task-handler state queue)]
+    (loop []
+      (Thread/sleep 200)
+      (step handler)
+      (recur))))
